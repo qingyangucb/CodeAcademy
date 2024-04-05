4d68f2c9c863acf081b64c300005862aeed4fc89
qingyangucb <qingyangucb@gmail.com>
2024-04-04 22:26:11 -0700

    finish rest_api

---
+++ project-4-boss-machine-start/server/api.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const apiRouter = express.Router();
+const {
+  createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase,
+} = require("./db");
+
+
+
+module.exports = apiRouter;
